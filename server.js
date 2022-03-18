b076c52d735d7c17256b857d81c881bca32a3229
Alaa <davelion3@gmail.com>
2022-03-18 10:11:55 +0300

    Implement the freecodecamp project specifications

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -17,6 +21,50 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const UrlShortenerSchema = new mongoose.Schema({
+  original_url: {
+    type: String,
+    required: true
+  },
+  short_url: {
+    type: Number,
+    required: true,
+    unique: true
+  }
+});
+const UrlShortener = mongoose.model('urlshortener', UrlShortenerSchema);
+
+app.post('/api/shorturl', async function(req, res) {
+  let result;
+  // console.log(req.body.url);
+  if (/^https?:\/\/.*$/.test(req.body.url)) {
+    const urlsShorteners = await UrlShortener.find({});
+    const newUrlShortener = new UrlShortener({
+      original_url: req.body.url,
+      short_url: urlsShorteners.length + 1
+    });
+    const newUrlShortenerResult = await newUrlShortener.save();
+    result = { original_url: newUrlShortenerResult.original_url, short_url: newUrlShortenerResult.short_url };
+  }
+  else {
+    result = { error: 'invalid url' };
+  }
+  res.json(result);
+});
+
+app.get('/api/shorturl/:short_url', async function(req, res) {
+  let urlShortener = await UrlShortener.findOne({ short_url: req.params.short_url });
+
+  if (urlShortener) {
+    res.redirect(302, urlShortener.original_url);
+  }
+  else {
+    res.json({ error: 'No short URL found for the given input' });
+  }
 });
 
 app.listen(port, function() {
